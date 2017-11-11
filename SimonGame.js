71b72b2110d058c1c4fb62d9d1981ec15e064750
xun13 <32128821+xun13@users.noreply.github.com>
2017-11-11 19:11:01 +0800

    Add files via upload

---
+++ SimonGame.js
@@ -0,0 +1,49 @@
+var c=["green_active","red_active","yellow_active","blue_active"];
+var time=null,arr=[],len;
+$("button").click(function(){
+   len =1;
+  start();
+})
+$(".item").click(function(){
+  var test = $(this).index();
+  if (test==arr[0]&&arr.length==1)
+    {
+      len++;
+      start();
+    }
+  else
+    {
+      if (test==arr[0])
+        arr.shift();
+      else
+       { 
+          $("p").text("xxx");
+         console.log("erro")
+         setTimeout(function(){
+           start();
+         },500);        
+       }
+    }
+})
+function start(){
+  $("p").text(len)
+  var i=0;
+  arr=[];
+  for (var j=0;j<len;j++){
+    arr.push(Math.floor(Math.random()*4));   
+  }
+  time=setInterval(function(){
+    if (i<arr.length){
+      shine(arr[i]);
+      i++;
+    }
+    else
+      clearInterval(time);
+  },1000)
+}
+function shine(ind){//闪烁
+  $(".item").eq(ind).addClass(c[ind]);
+  setTimeout(function(){
+    $(".item").eq(ind).removeClass(c[ind]);
+  },300)
+}
